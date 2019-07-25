b2cee12ae74b0e4e0cb391268ae736da63856aea
Jason Tsang <jasontsang.dev@gmail.com>
2019-07-25 08:55:47 +0800

    :tada: optimise.

--- src/app/shared/cors-interceptor.ts
+++ src/app/shared/cors-interceptor.ts
@@ -4,14 +4,13 @@ import {
 } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
-import isElectron from 'is-electron';
 
 @Injectable()
 export class CorsInterceptor implements HttpInterceptor {
     private proxy = 'https://www.jasontsang.dev/proxy/?url=';
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (isElectron() && req.url.includes(this.proxy)) {
+        if (req.url.includes(this.proxy)) {
             return next.handle(req.clone({
                 url: decodeURIComponent(req.url.slice(this.proxy.length))
             }));
